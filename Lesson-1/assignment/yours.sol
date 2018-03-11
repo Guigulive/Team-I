c60733e8f2a9882bd4b6c5e5461c436f7da5c069
outmyth <593675+outmyth@users.noreply.github.com>
2018-03-11 01:22:30 -0800

    Update yours.sol

--- Lesson-1/assignment/yours.sol
+++ Lesson-1/assignment/yours.sol
@@ -1,1 +1,55 @@
-/*作业请提交在这个目录下*/
+pragma solidity ^0.4.14;
+
+contract Payroll {
+    // employer info
+    address employer;
+    
+    // employee info
+    // TODO: those fileds could be encapsulated into a struct
+    uint salary;
+    address employee;
+    uint lastPayday;
+    
+    uint constant payDuration = 2 seconds;
+    
+    function Payroll() {
+        employer = msg.sender;
+    }
+
+    function setEmployee(address _employee, uint _salary) {
+        // only the employer can decide the salary of employees
+        if(msg.sender != employer) {
+            revert();
+        }
+        
+        employee = _employee;
+        salary = _salary * 1 ether;
+        lastPayday = now;
+    }
+    
+    function addFund() payable returns (uint) {
+        return this.balance;
+    }
+    
+    function calculateRunway() returns (uint) {
+        return this.balance / salary;
+    }
+    
+    function hasEnoughFund() returns (bool) {
+        return calculateRunway() > 0;
+    }
+    
+    function getPaid() {
+        if(msg.sender != employee) {
+            revert();
+        }
+        
+        uint nextPayday = lastPayday + payDuration;
+        if(nextPayday > now) {
+            revert();
+        }
+        
+        lastPayday = nextPayday;
+        employee.transfer(salary);
+    }
+}
